4efe031296626dbcc74ab7ae132a23b981a4a125
princeemeka965 <anyanwue4@gmail.com>
2023-09-30 13:09:31 +0100

    build side bar

---
+++ src/SideBar.tsx
@@ -0,0 +1,24 @@
+import { Hamburger } from "./icons/icons";
+
+const SideBar: React.FC = () => {
+  return (
+    <>
+      <div
+        className="h-full flex flex-col p-2 bg-white shadow-md fixed"
+        style={{ height: "100vh", width: "3%" }}
+      >
+        <div className="w-full flex justify-center p-2 mt-2 mb-16">
+          <Hamburger />
+        </div>
+        <div className="w-full flex justify-center p-2 my-2">
+          <img src="./home-icon.png" alt="home" />
+        </div>
+        <div className="w-full flex justify-center p-2 my-2">
+          <img src="./todoList.png" alt="todoList" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
---
+++ src/icons/icons.tsx
@@ -0,0 +1,14 @@
+export const Hamburger = (props: any) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={23}
+    height={19}
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#000"
+      d="M.768.309h22v2.924h-22zM.768 8.106h22v2.924h-22zM.768 15.903h22v2.924h-22z"
+    />
+  </svg>
+);
